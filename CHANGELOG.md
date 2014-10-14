56c22d79c7d7a8bdaa701952ea22e426738432df
Greg Albrecht <gba@onbeep.com>
2014-10-13 21:41:12 -0700

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of weewx
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'weewx'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures weewx'
+long_description 'Installs/Configures weewx'
+version          '0.1.0'
+
+depends 'subversion'
